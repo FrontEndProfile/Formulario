fc07e7ab4b32b47655edf7e0fd310dff83588b6c
VigoFazal <frontendprofile@gmail.com>
2022-10-10 05:38:49 -0700

    v-02

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,27 @@
-// var currentTab = 0; // Current tab is set to be the first tab (0)
-// showTab(currentTab); // Display the current tab
-
-// function showTab(n) {
-//   // This function will display the specified tab of the form...
-//   var x = document.getElementsByClassName("tab");
-//   x[n].style.display = "block";
-//   //... and fix the Previous/Next buttons:
-//   if (n == 0) {
-//     document.getElementById("prevBtn").style.display = "none";
-//   } else {
-//     document.getElementById("prevBtn").style.display = "inline";
-//   }
-//   if (n == (x.length - 1)) {
-//     document.getElementById("nextBtn").innerHTML = "Submit";
-//   } else {
-//     document.getElementById("nextBtn").innerHTML = "Next";
-//   }
-//   //... and run a function that will display the correct step indicator:
-//   fixStepIndicator(n)
-// }
-
-// function nextPrev(n) {
-//   // This function will figure out which tab to display
-//   var x = document.getElementsByClassName("tab");
-//   // Exit the function if any field in the current tab is invalid:
-//   if (n == 1 && !validateForm()) return false;
-//   // Hide the current tab:
-//   x[currentTab].style.display = "none";
-//   // Increase or decrease the current tab by 1:
-//   currentTab = currentTab + n;
-//   // if you have reached the end of the form...
-//   if (currentTab >= x.length) {
-//     // ... the form gets submitted:
-//     document.getElementById("regForm").submit();
-//     return false;
-//   }
-//   // Otherwise, display the correct tab:
-//   showTab(currentTab);
-// }
-
-// function validateForm() {
-//   // This function deals with validation of the form fields
-//   var x, y, i, valid = true;
-//   x = document.getElementsByClassName("tab");
-//   y = x[currentTab].getElementsByTagName("label");
-//   // A loop that checks every input field in the current tab:
-//   for (i = 0; i < y.length; i++) {
-//     // If a field is empty...
-//     if (y[i].value == "") {
-//       // add an "invalid" class to the field:
-//       y[i].className += " invalid";
-//       // and set the current valid status to false
-//       valid = false;
-//     }
-//   }
-//   // If the valid status is true, mark the step as finished and valid:
-//   if (valid) {
-//     document.getElementsByClassName("step")[currentTab].className += " finish";
-//   }
-//   return valid; // return the valid status
-// }
-
-// function fixStepIndicator(n) {
-//   // This function removes the "active" class of all steps...
-//   var i, x = document.getElementsByClassName("step");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].className = x[i].className.replace(" active", "");
-//   }
-//   //... and adds the "active" class on the current step:
-//   x[n].className += " active";
-// }
-
-// document.querySelector("header").style.background = "red"
-// document.querySelector(".form-check-input:checked[type="checkbox"]").style.background = "red";
+function myFunction() {
+  var checkBox = document.getElementById("myCheck1");
+  if (checkBox.checked == true){
+    document.querySelector(".form_btn_next" ).className = "form_btn_next active";
+    document.querySelector(".form_btn_next.active").disabled = false;
+    
+  } else {
+    document.querySelector(".form_btn_next").className = "form_btn_next noactive";
+    document.querySelector(".form_btn_next.noactive").disabled = true;
+  }
+}
+// === 
+function myInfo() {
+  var checkBox = document.getElementById("changeinfo");
+  if (checkBox.checked){
+    document.querySelector(".img-fluid.change__img").src='assets/img/humano.svg';
+    document.querySelector(".form_row_small").innerHTML = "Nombre y apellido";
+  } else {
+    document.querySelector(".img-fluid.change__img").src='assets/img/ars-logo.svg';
+    document.querySelector(".form_row_small").innerHTML = "Solicitud de autenticación";
+  }
+}
+// === 
+function loader() {
+  document.querySelector(".full_loader").style.display = "block" ;
+  
+}
